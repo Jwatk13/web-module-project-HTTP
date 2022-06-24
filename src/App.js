d1835a0deafbead4839befb8f584aea52f32280c
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-23 22:21:16 -0400

    Delete movie functionality is working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
 import EditMovieForm from './components/EditMovieForm';
+import AddMovieForm from './components/AddMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
@@ -24,7 +25,14 @@ const App = (props) => {
       });
   }, []);
 
+
+//this is what we need to update our state for movie deletion
+//the Number before our id makes sure that we get a number instead of a string since the id is currently a string.
+//filter recieves logic that looks at the id that we provide when we click the delete button and matches that id to the correct movie in the database so we can delete the proper one and it is recorded in state.
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item => 
+      item.id !== Number(id)
+    ));
   }
 
   const addToFavorites = (movie) => {
@@ -43,12 +51,17 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies}/>
         
           <Switch>
+            <Route path="/movies/add">
+              <AddMovieForm />
+            </Route>
+
             <Route path="/movies/edit/:id">
               <EditMovieForm  setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              {/* //Now to pass in our delete movie function to the component that needs it */}
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
